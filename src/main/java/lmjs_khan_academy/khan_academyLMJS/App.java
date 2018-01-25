b81026e57e1991bfe55ffd7efcbd6f2bc6d2a606
tr_lnd_user <tr_lnd_user@LND-DS36D2S-DX.dir.svc.accenture.com>
2018-01-25 13:47:47 +0800

    Initial files

---
+++ src/main/java/lmjs_khan_academy/khan_academyLMJS/App.java
@@ -0,0 +1,13 @@
+package lmjs_khan_academy.khan_academyLMJS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

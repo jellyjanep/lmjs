a81042de5225e1473b0e240891bd3762db5db7ce
tr_lnd_user <tr_lnd_user@LND-DS36D2S-DX.dir.svc.accenture.com>
2018-01-25 14:14:39 +0800

    added packages and classes under main and test

---
+++ src/main/java/pageobjects/Main.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/teardown/Teardown.java
@@ -0,0 +1,5 @@
+package teardown;
+
+public class Teardown {
+
+}
